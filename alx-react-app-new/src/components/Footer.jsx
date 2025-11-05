1aca5c7755b033308c51a57b2bcd6bad1d845299
James <qjaymce37@gmail.com>
2025-11-05 19:18:08 +0000

    modify files

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+
+return (
+<footer>
+  <p style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>© 2023 City Lovers</p>
+</footer>)
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+return (
+<main>
+  <p style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+);
+}
+
+export default MainContent;
