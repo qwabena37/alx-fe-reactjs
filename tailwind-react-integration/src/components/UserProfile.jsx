3361026c452ce6a97152d5c72435170d6a1cf516
James <qjaymce37@gmail.com>
2025-11-27 10:58:28 +0000

    customize content

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,16 +1,18 @@
+import profilePic from "../assets/me.jpg";
+
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
-        src="https://via.placeholder.com/150"
+        src={profilePic}
         alt="User"
         className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
-        John Doe
+        James Appiah
       </h1>
       <p className="text-sm md:text-base text-gray-600">
-        Developer at Example Co. Loves to write code and explore new technologies.
+        Developer at JayTech Solutions. Loves to write code and explore new technologies.
       </p>
     </div>
   );
