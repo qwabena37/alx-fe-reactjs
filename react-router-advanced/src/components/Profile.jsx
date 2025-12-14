6c15496cc0f2c69eef7c1b679cedb8ab98057461
James <qjaymce37@gmail.com>
2025-12-14 18:45:31 +0000

    modify Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,7 @@
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 function Profile() {
   return (
@@ -10,7 +13,11 @@ function Profile() {
         <Link to="settings">Profile Settings</Link>
       </nav>
 
-      <Outlet />
+      {/* Nested Routes declared here */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
