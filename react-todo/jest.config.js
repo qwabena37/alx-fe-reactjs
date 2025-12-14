ad71a827132081e073137a9446b0843239fa50ad
James <qjaymce37@gmail.com>
2025-12-14 20:00:43 +0000

    modify test name

--- react-todo/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  moduleFileExtensions: ['js', 'jsx'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  testMatch: ['**/__tests__/**/*.test.jsx', '**/?(*.)+(spec|test).jsx'],
-};
